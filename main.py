da83bee915d90014451176420f7901936dd05b04
Konstantinos <58148103+konmaz@users.noreply.github.com>
2020-04-19 00:31:53 +0300

    Change thumbnail when click download button

--- main.py
+++ main.py
@@ -1,43 +1,25 @@
 import PySimpleGUI as sg
+import re
 import urllib
-import urllib.parse as urlparse
-def get_video_id(url):
-    """Returns Video_ID extracting from the given url of Youtube
-    
-    Examples of URLs:
-      Valid:
-        'http://youtu.be/_lOT2p_FCvA',
-        'www.youtube.com/watch?v=_lOT2p_FCvA&feature=feedu',
-        'http://www.youtube.com/embed/_lOT2p_FCvA',
-        'http://www.youtube.com/v/_lOT2p_FCvA?version=3&amp;hl=en_US',
-        'https://www.youtube.com/watch?v=rTHlyTphWP0&index=6&list=PLjeDyYvG6-40qawYNR4juzvSOg-ezZ2a6',
-        'youtube.com/watch?v=_lOT2p_FCvA',
-      
-      Invalid:
-        'youtu.be/watch?v=_lOT2p_FCvA',
-    """
 
-    from urlparse import urlparse, parse_qs
+def get_video_id(youtube_url):
+    regex = re.compile(r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/(watch\?v=|embed/|v/|.+\?v=)?(?P<id>[A-Za-z0-9\-=_]{11})')
 
-    if url.startswith(('youtu', 'www')):
-        url = 'http://' + url
-        
-    query = urlparse(url)
+    match = regex.match(youtube_url)
+
+    if not match:
+        print('no match')
+    return(match.group('id'))
+def get_video_thumb(yt_id):
+    return 'http://img.youtube.com/vi/'+yt_id+'/maxresdefault.jpg'
     
-    if 'youtube' in query.hostname:
-        if query.path == '/watch':
-            return parse_qs(query.query)['v'][0]
-        elif query.path.startswith(('/embed/', '/v/')):
-            return query.path.split('/')[2]
-    elif 'youtu.be' in query.hostname:
-        return query.path[1:]
-    else:
-        raise ValueError
+image_elem = sg.Image(filename='button.png')
+
    # Add a little color to your windows
 # All the stuff inside your window. This is the PSG magic code compactor...
 
 layout = [  [sg.Image(filename="logo.png",background_color='#3a3a3a',pad=(25,25,25,25))],
-            
+            [image_elem],
             [sg.Text('Enter something on Row 2'), sg.InputText(key="txt_url")], # URL box
             [sg.Button(image_filename="button.png",button_color=('#3a3a3a', '#3a3a3a'),border_width=0,tooltip="Download",key='btn_dwn')], # download button
             
@@ -58,8 +40,10 @@ while True:
     if event in (None, 'Cancel'):
         break
     elif event in 'btn_dwn':
-        url = values['txt_url'];
-        print (get_video_id(url))
+        #url = values['txt_url'];
+        #video_id =  (get_video_id(url))
+        #print (get_video_thumb(video_id))
+        image_elem.Update(filename='logo.png') 
         
 
 window.close()
